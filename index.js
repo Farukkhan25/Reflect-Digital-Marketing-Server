53c4cfedfda6ea4f1f0a60c388d009949062c9af
Farukkhan25 <programmingherofaruk@gmail.com>
2022-11-08 22:29:30 +0600

    Services api added

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_USER);
+
+// middle wares
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qbydxwz.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
@@ -14,12 +18,40 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
+async function run() { 
+  try {
+    const serviceCollection = client
+      .db("digitalMarketing")
+      .collection("services");
+    
+    app.get("/", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    }
+  finally {
+
+  }
+
+  }
+  
+  run().catch(err => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("server is running");
