b8162d6d30e15cf78288428bef98f8c5c91b4b1a
Farukkhan25 <programmingherofaruk@gmail.com>
2022-11-09 11:16:28 +0600

    Services limit api added for Homepage

--- index.js
+++ index.js
@@ -12,46 +12,44 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qbydxwz.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+console.log(uri);
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-// client.connect((err) => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
 
-async function run() { 
+
+async function run() {
   try {
-    const serviceCollection = client
-      .db("digitalMarketing")
-      .collection("services");
-    
+    const serviceCollection = client.db('digitalMarketing').collection('services');
+
     app.get("/", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
-      const services = await cursor.toArray();
-      res.send(services);
+      const allServices = await cursor.limit(3).toArray();
+      res.send(allServices);
     });
 
-    app.get("/services", async (req, res) => {
+    app.get("/details", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
-      const services = await cursor.toArray();
-      res.send(services);
+      const allServices = await cursor.toArray();
+      res.send(allServices);
     });
 
-    }
-  finally {
-
+    app.get("/details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+  } finally {
   }
+}
 
-  }
-  
-  run().catch(err => console.error(err));
+run().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("server is running");
